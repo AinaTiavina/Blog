e518349b0187120835b6e53f6b855a3397b5d400
Aina <rajoelisonainatiavina@gmail.com>
2021-11-30 18:28:04 +0300

    create form class, add create method for postcontroller, add new template

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,7 +18,30 @@ class PostController extends AbstractController
      */
     public function index(PostRepository $postRep): Response
     {
-        $posts = $postRep->findAll();
+        $posts = $postRep->findBy([],['CreatedAt' => 'DESC']);
+        
         return $this->render('post/index.html.twig', compact('posts'));
     }
+
+    /**
+     * @Route("/create", name="app_create", methods={"GET","POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $post = new Post;
+        $form = $this->createForm(PostType::class, $post);
+        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute("app_home");
+        }
+
+        return $this->render('post/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }    
 }
