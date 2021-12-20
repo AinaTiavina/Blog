1203ff45b78bbdc11ccd196015ba0cb932498e28
Aina <rajoelisonainatiavina@gmail.com>
2021-12-20 21:24:42 +0300

    create a voter to verify if the user is a owner of a post

---
+++ src/Security/Voter/PostVoter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class PostVoter extends Voter
+{
+    protected function supports(string $attribute, $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return $attribute == 'OWNER'
+            && $subject instanceof \App\Entity\Post;
+    }
+
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case 'OWNER':
+                return $user && $user == $subject->getUser();
+        }
+
+        return false;
+    }
+}
